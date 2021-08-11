d29840f7ffdc3fbaf7afd1772364678a26d98c85
Aditya <adityasghadge@student.sfit.ac.in>
2021-08-11 09:16:09 +0530

    task completed

--- index.php
+++ index.php
@@ -75,7 +75,8 @@
 			<div class="text-center">&copy; Copyright Punjab National Bank India 2021</div>
 		</div>
 	</footer>
-    
+
+  	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>  
   	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
